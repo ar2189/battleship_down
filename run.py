b20318562b294d9f29a4e37319594a9f3002c841
ar2189 <atif2189.ar@gmail.com>
2022-12-11 21:41:25 +0000

    Define the board function to represent x as rows and y as columns

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ import random as rand
 
 class Game(object):
     """
-    Define the game class to make it easier to understand
+    Main board class. Sets board size, the number of players
+    and the ships placed randomely.
     """
     def __init__(self, players):
         self.guesses = 5
@@ -22,3 +23,26 @@ class Game(object):
         self.ship_col = rand.randint(0, 4)
         self.guess_row = 0
         self.guess_col = 0
+
+
+    def print_board(self, board_in):
+        """
+        Defining the print board.
+        x represents rows.
+        y represents columns.
+        """
+        x = 0
+        y = 0
+        for column in board_in:
+            y = 0
+            for row in column:
+                if y == 0:
+                    print(" ", row, end=" ")
+                elif y == len(board_in[x]):
+                    print("", row, end=""):
+                else:
+                    print(row, end=" ")
+                y += 1
+            print()
+            x += 1
+        return None    
