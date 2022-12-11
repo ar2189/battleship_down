02daabfd6509aea1e39f93cc2b7f119070b40784
ar2189 <atif2189.ar@gmail.com>
2022-12-11 20:54:02 +0000

    Create a class for the game to see how many players are playing

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Battleship
+
+# Imports
+import copy as c
+import os
+import random as rand
+
+
+class Game(object):
+    """
+    Define the game class to make it easier to understand
+    """
+    def __init__(self, players):
+        self.guesses = 5
+        self.player_list = []
+        for player in range(players):
+            self.player_list.append(self.guesses)
+        self.current_player = 1
+        self.board = self.creatematrix(5, 5)
+        self.board_visible = c.deepcopy(self.board)
+        self.ship_row = rand.randint(0, 4)
+        self.ship_col = rand.randint(0, 4)
+        self.guess_row = 0
+        self.guess_col = 0
