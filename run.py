4b2d37bb30f0fac19ae34c51387af2726f616f43
ar2189 <atif2189.ar@gmail.com>
2022-12-13 20:55:29 +0000

    Create a funtion that will tell us how many ships have been hit or if we missed

--- run.py
+++ run.py
@@ -41,7 +41,7 @@ def create_ships(board):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
         while board[ship_row][ship_column] == 'X':
             ship_row, ship_column = get_ship_location()
-        board[ship_row][ship_column = 'X']
+        board[ship_row][ship_column] = "X"
 
 
 def get_ship_location():
@@ -54,4 +54,42 @@ def get_ship_location():
         print("Not an appropriate choice, please select again")
         column = input("Enter the column of the ship:").upper()
 
-    return int(row) - 1, letters_to_numbers[column]    
+    return int(row) - 1, letters_to_numbers[column]
+
+
+def count_hit_ships(board):
+    """
+    This Function will check how many ships have been hit
+    """
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    create_ships(HIDDEN_BOARD)
+    turns = 10
+    while turns > 0:
+        print("Guess a battleship location")
+        print_board(GUESS_BOARD)
+        row, column = get_ship_location()
+        if GUESS_BOARD[row][column] == "-":
+            print("You guessed that one already.")
+        elif HIDDEN_BOARD[row][column] == "X":
+            print("HIT!")
+            GUESS_BOARD[row][column] = "X"
+            turns -= 1
+        else:
+            print("MISS!")
+            GUESS_BOARD[row][column] = "-"
+            turns -= 1
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print("You Win!")
+            break
+        print("You have " + str(turns) + " turns left")
+        if turns == 0:
+            print("You ran out of turns, GAME OVER!")
